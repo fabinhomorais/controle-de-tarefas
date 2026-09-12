fdbff8847ef0171379ee4501acdb96a5b352bdbf
Fábio Morais <fabiomorais95@gmail.com>
2026-09-12 10:25:11 -0300

    adiciona leitura dos campos da tarefa via console

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const today = new Date();
 document.getElementById("week").textContent = "Hoje é " + today.toLocaleDateString("pt-BR");
+
+const task = {
+  category: "Trabalho",
+  title: "Cotação João — Recife/Gramado",
+  time: "30 min",
+  priority: "high"
+};
+
+const message =  `A tarefa é: ${task.title}`;
+console.log(message);
