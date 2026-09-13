5a5c6dd61a78151bca597128d70268d6aff8fa8e
Fábio Morais <fabiomorais95@gmail.com>
2026-09-13 01:29:48 -0300

    usa verde na prioridade baixa

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ const task = {
   category: "Trabalho",
   title: "Cotação João — Recife/Gramado",
   time: "30 min",
-  priority: "high"
+  priority: "low"
 };
 
+let priorityLabel;
+let priorityClass;
+
+if (task.priority === "high") {
+  priorityLabel = "Alta";
+  priorityClass = "priority-high";
+} else if (task.priority === "medium") {
+  priorityLabel = "Média";
+  priorityClass = "priority-medium";
+} else {
+  priorityLabel = "Baixa";
+  priorityClass = "priority-low";
+}
 
 const taskList = document.getElementById("task-list");
 
@@ -16,6 +29,6 @@ taskList.innerHTML = `
     <p class="task-category">${task.category}</p>
     <p class="task-tittle">${task.title}</p>
     <p class="task-time">${task.time}</p>
-    <span class="priority priority-high">${task.priority}</span>
+    <span class="priority ${priorityClass}">${priorityLabel}</span>
   </div>
 `;
